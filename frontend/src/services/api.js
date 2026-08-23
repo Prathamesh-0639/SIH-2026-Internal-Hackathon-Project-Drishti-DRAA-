696990fb10e5e2a1a92683c91c74250613aaedb3
Prathamesh Ananda Powar <prathameshpowar524@gmail.com>
2026-08-23 18:36:36 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://sih-2026-internal-hackathon-project-9y88.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
